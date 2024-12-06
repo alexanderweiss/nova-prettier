4f149ca4cdf8fa3b8f5b910462c99cf83692bc7b
Toni Förster <toni.foerster@icloud.com>
2024-12-06 13:25:34 +0100

    use a unified config file for building configs
    Signed-off-by: Toni Förster <toni.foerster@icloud.com>

--- rollup.config.js
+++ rollup.config.js
@@ -3,115 +3,58 @@ import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import fs from 'fs'
 
-// Load configurations
-const commonConfig = JSON.parse(
-	fs.readFileSync('./src/configCommon.json', 'utf8'),
-)
-const specificConfig = JSON.parse(
-	fs.readFileSync('./src/configSpecific.json', 'utf8'),
-)
-const workspaceSpecificConfig = JSON.parse(
-	fs.readFileSync('./src/configWorkspaceSpecific.json', 'utf8'),
+// Load unified configuration
+const unifiedConfig = JSON.parse(
+	fs.readFileSync('./src/unifiedConfig.json', 'utf8'),
 )
 
-// Default properties for ignored syntaxes
-const ignoredSyntaxDefaultsForConfig = {
-	type: 'boolean',
-	default: false,
-}
+// Function to extract specific configurations
+const extractConfig = (unifiedConfig, type) => {
+	const extract = (item) => {
+		const extracted = { ...item }
+		delete extracted.config
+		delete extracted.configWorkspace
 
-const ignoredSyntaxDefaultsForWorkspace = {
-	type: 'enum',
-	radio: false,
-	values: ['Global Setting', 'Enabled', 'Disabled'],
-	default: 'Global Setting',
-}
-
-// Function to merge configurations and apply defaults
-const mergeConfigSections = (
-	commonSections,
-	specificSections,
-	defaults = {},
-) => {
-	const specificSectionMap = Object.fromEntries(
-		specificSections.map((section) => [section.key, section]),
-	)
-
-	return commonSections.map((commonSection) => {
-		const specificSection = specificSectionMap[commonSection.key] || {}
-
-		// Merge common and specific sections
-		const mergedSection = {
-			...commonSection,
-			...specificSection,
+		if (type === 'config' && item.config) {
+			Object.assign(extracted, item.config)
+		} else if (type === 'configWorkspace' && item.configWorkspace) {
+			Object.assign(extracted, item.configWorkspace)
 		}
 
-		// Apply defaults to `ignored-syntaxes` keys
-		if (
-			mergedSection.key.startsWith(
-				'prettier.format-on-save.ignored-syntaxes.',
-			) &&
-			mergedSection.key !== 'prettier.format-on-save.ignored-syntaxes.section'
-		) {
-			for (const [key, value] of Object.entries(defaults)) {
-				if (mergedSection[key] === undefined) {
-					mergedSection[key] = value
-				}
-			}
+		if (item.children) {
+			extracted.children = item.children.map(extract)
 		}
 
-		// Recursively process children if present
-		if (commonSection.children || specificSection.children) {
-			mergedSection.children = mergeConfigSections(
-				commonSection.children || [],
-				specificSection.children || [],
-				defaults,
-			)
-		}
+		return extracted
+	}
 
-		return mergedSection
-	})
+	return unifiedConfig.map(extract)
 }
 
 // Generate configurations
-const generateConfig = (commonConfig, specificConfig, defaults) =>
-	mergeConfigSections(commonConfig, specificConfig, defaults)
-
-const generateWorkspaceConfig = (commonConfig, workspaceConfig, defaults) =>
-	mergeConfigSections(commonConfig, workspaceConfig, defaults)
-
-// Create final configurations
-const finalConfig = generateConfig(
-	commonConfig,
-	specificConfig,
-	ignoredSyntaxDefaultsForConfig,
-)
-const finalWorkspaceConfig = generateWorkspaceConfig(
-	commonConfig,
-	workspaceSpecificConfig,
-	ignoredSyntaxDefaultsForWorkspace,
-)
+const globalConfig = extractConfig(unifiedConfig, 'config')
+const workspaceConfig = extractConfig(unifiedConfig, 'configWorkspace')
 
 // Write the configurations to files
 fs.writeFileSync(
 	'./prettier.novaextension/config.json',
-	JSON.stringify(finalConfig, null, 2),
+	JSON.stringify(globalConfig, null, 2),
 )
 fs.writeFileSync(
 	'./prettier.novaextension/configWorkspace.json',
-	JSON.stringify(finalWorkspaceConfig, null, 2),
+	JSON.stringify(workspaceConfig, null, 2),
 )
 
 // Minify JSON files
-const flattenAndMinifyFile = (filePath) => {
+const minifyConfigFile = (filePath) => {
 	const data = JSON.parse(fs.readFileSync(filePath, 'utf8')) // Read and parse JSON
 	const minified = JSON.stringify(data) // Minify the JSON
 	fs.writeFileSync(filePath, minified) // Write back the minified JSON
 }
 
 // Minify the output files
-flattenAndMinifyFile('./prettier.novaextension/config.json')
-flattenAndMinifyFile('./prettier.novaextension/configWorkspace.json')
+minifyConfigFile('./prettier.novaextension/config.json')
+minifyConfigFile('./prettier.novaextension/configWorkspace.json')
 
 export default [
 	{
